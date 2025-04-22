a46efd06b0e0b5ba38d2b0c33a446f41b704f174
marlonprado2025 <marlon123po2@gmail.com>
2025-04-22 02:06:37 -0300

    update script

---
+++ Script.js
@@ -0,0 +1,15 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const chamaMascara = document.querySelector(".mascaraForm")
+
+
+function cliqueinobotao() {
+   form.style.left = "50%"
+   form.style.transform = "translateX(-50%)"
+   chamaMascara.style.visibility = "visible"
+}
+
+function cliqueMascara() {
+   chamaMascara.style.visibility = "hidden"
+   form.style.left = "-298px"
+   form.style.top= "30%"
+}
