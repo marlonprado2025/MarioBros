059d5ff894129436e70164f2c565f93deacae549
marlonprado2025 <marlon123po2@gmail.com>
2025-04-24 00:07:46 -0300

    update reposividade

--- Script.js
+++ Script.js
@@ -1,15 +1,21 @@
-const form = document.querySelector(".formulario-fale-conosco")
-const chamaMascara = document.querySelector(".mascaraForm")
-
+const form = document.querySelector(".formulario-fale-conosco");
+const chamaMascara = document.querySelector(".mascaraForm");
+const faleConosco = document.querySelector(".faleConosco");
 
 function cliqueinobotao() {
-   form.style.left = "50%"
-   form.style.transform = "translateX(-50%)"
-   chamaMascara.style.visibility = "visible"
+  form.style.left = "50%";
+  form.style.transform = "translateX(-50%)";
+  chamaMascara.style.visibility = "visible";
 }
 
 function cliqueMascara() {
-   chamaMascara.style.visibility = "hidden"
-   form.style.left = "-298px"
-   form.style.top= "30%"
+  chamaMascara.style.visibility = "hidden";
+  form.style.left = "-298px";
+  form.style.top = "30%";
+}
+
+function abrirForm() {
+  form.style.left = "50%";
+  form.style.transform = "translateX(-50%)";
+  chamaMascara.style.visibility = "visible";
 }
